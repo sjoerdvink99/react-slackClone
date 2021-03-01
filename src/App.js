96f020a35448bf0bd7f254b516f95636b8e32558
Sjoerd Vink <sjoerdvink@icloud.com>
2021-03-01 09:27:03 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const tijd = "00.10.00";
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Slack</h1>
+    </div>
+  );
+}
+
+export default App;
